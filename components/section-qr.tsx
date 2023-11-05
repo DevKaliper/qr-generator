c7b0f10995e4bf218e75006e7e5723e21600d065
Dev Kaliper <gxlthz@gmail.com>
2023-11-05 15:27:42 -0500

    Add animation and update code heading in SectionQR component

--- components/section-qr.tsx
+++ components/section-qr.tsx
@@ -1,7 +1,12 @@
 export default function SectionQR() {
     return (
         <article className="h-full w-full lg:w-2/5 flex flex-col justify-center items-center border-2 shadow-lg dark:border-x-zinc-600  border-gray-950 rounded-lg">
-            <h2>code</h2>
+            <h2 className="font-semibold lg:text-2xl my-3">Your Magnificant code will appear here :)</h2>
+            <div>
+                <span>animation</span>
+                
+            </div>
+          
         </article>
     )
 
